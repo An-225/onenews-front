756cf25679eda49d4548b0e24e89c059d3d77c63
Diego Candido <diego.candido@onebox.one>
2023-11-21 18:10:00 -0300

    change host back end

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -10,7 +10,7 @@ export class SearchService {
   constructor(private http: HttpClient) {}
 
   searchTopic(topic: string, from: string, to: string): Observable<any> {
-    const url = 'http://localhost:8080/api/v1/search';
+    const url = 'https://ivory-enigma-405820.uc.r.appspot.com/api/v1/search';
     const params = new HttpParams()
       .set('topic', topic)
       .set('from', from)
