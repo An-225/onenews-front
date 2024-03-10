8b44517f2a2fb6f7abb9df0f8d4c017b638ee9bf
An-225 <66622436+An-225@users.noreply.github.com>
2024-03-10 17:33:41 -0300

    clear search-bar component

--- src/app/components/search/search-bar.component.ts
+++ src/app/components/search/search-bar.component.ts
@@ -1,12 +1,8 @@
-import {Component, HostListener, ViewChild} from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 import {SharedService} from '../../services/shared-service.service';
 import {SearchService} from 'src/app/services/search.service';
 import * as moment from 'moment';
-import {FormGroup} from "@angular/forms";
 import {Moment} from "moment";
-import {MatDialog} from "@angular/material/dialog";
-import {SourceComponent} from "../source/source.component";
-import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
     selector: 'search',
@@ -16,18 +12,14 @@ import {MatSnackBar} from "@angular/material/snack-bar";
 export class SearchBarComponent {
     searchText: string = '';
     showProgressBar: boolean = false;
-    showDateRangeSelector: boolean = false;
-    showSortSelector: boolean = false;
     dynamicSearch: string[] = [];
-    minStartDate: Moment = moment().subtract(1, 'months');
-    maxEndDate: Moment = moment();
     startDate: Moment = moment().subtract(1, 'months');
     endDate: Moment = moment();
     sortMethod: string = 'relevancy'
     selectedSources: string[] = [];
     sources: any[] = [];
 
-    constructor(private sharedService: SharedService, private searchService: SearchService, private dialog: MatDialog, private snackBar: MatSnackBar) {
+    constructor(private sharedService: SharedService, private searchService: SearchService) {
         this.sharedService.getDynamicSearchObservable().subscribe((dynamicSearch: string[]) => {
             this.dynamicSearch = dynamicSearch
         });
@@ -66,7 +58,6 @@ export class SearchBarComponent {
             const status = data.status;
 
             if (status === 'ok') {
-                const totalResults = data.totalResults;
                 const articles = data.articles;
                 this.showProgressBar = false;
                 this.sharedService.setShowCards(true);
@@ -81,53 +72,5 @@ export class SearchBarComponent {
                 this.showProgressBar = false;
             }
         });
-    }
-
-
-    openDateRangeSelector() {
-        this.showDateRangeSelector = true;
-    }
-
-    onDateRangeSelected(range: FormGroup) {
-        let tempStartDate = moment(range.value.start);
-        let tempEndDate = moment(range.value.end);
-        if (tempStartDate.isBefore(this.minStartDate) || tempEndDate.isAfter(this.maxEndDate)) {
-            this.openSnackBar('O período máximo é de 1 meŝ', 'fechar')
-            this.showDateRangeSelector = false;
-            return;
-        }
-
-        this.startDate = tempStartDate
-        this.endDate = tempEndDate
-    }
-
-    openSortSelector() {
-        this.showSortSelector = true;
-    }
-
-    openSourceSelector() {
-        const dialogRef = this.dialog.open(SourceComponent,
-            {
-                data: {
-                    'sources': this.sources,
-                    'selectedSources': this.selectedSources
-                }
-            });
-        dialogRef.componentInstance.sourcesSelected.subscribe(value => this.onSourceSelected(value))
-        dialogRef.afterClosed().subscribe(() => {
-        });
-    }
-
-    onSortMethodSelected(method: String) {
-        this.sortMethod = method.toString()
-        this.showSortSelector = false;
-    }
-
-    onSourceSelected(selectedSources: string[]) {
-        this.selectedSources = selectedSources
-    }
-
-    openSnackBar(message: string, action: string) {
-        this.snackBar.open(message, action);
     }
 }
